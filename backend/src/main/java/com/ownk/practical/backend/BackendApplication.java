ac539023e2138a38c985df6ae1ac100bedcb8d85
Saúl Quintero <saqpsaqp@gmail.com>
2017-07-23 07:54:45 -0400

    Added static index

--- backend/src/main/java/com/ownk/practical/backend/BackendApplication.java
+++ backend/src/main/java/com/ownk/practical/backend/BackendApplication.java
@@ -6,13 +6,25 @@ import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@MappedTypes(Vendedor.class)
+@Configuration
+@ComponentScan
 @MapperScan("com.ownk.practical.backend.mapper")
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+        
+        @Override
+        protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+            return application.sources(BackendApplication.class);
+        
+        }
+        
 }
--- backend/src/main/java/com/ownk/practical/backend/controller/HomeController.java
+++ backend/src/main/java/com/ownk/practical/backend/controller/HomeController.java
@@ -1,13 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.ownk.practical.backend.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+/**
+ *
+ * @author squintero
+ */
+@Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String home(){
-        return "Hola Mundo";
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homeController(){
+        return "index";
     }
     
 }
